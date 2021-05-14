feb93c2f7effb51b92f458ac004782371594a1fa
jhkim <hansalim76@gmail.com>
2021-05-14 14:37:05 +0900

    mustache 예제 추가

--- src/main/java/com/naoek/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/naoek/springboot/domain/posts/PostsRepository.java
@@ -1,6 +1,9 @@
 package com.naoek.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 /*
  * - JpaRepository를 상속하면 기본적인 CRUD 메소드가 자동으로 생성됨.
@@ -9,4 +12,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
  */
 public interface PostsRepository extends JpaRepository<Posts, Long> {
 
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 }
--- src/main/java/com/naoek/springboot/web/PostsApiController.java
+++ src/main/java/com/naoek/springboot/web/PostsApiController.java
@@ -3,6 +3,7 @@ package com.naoek.springboot.web;
 import com.naoek.springboot.service.PostsService;
 import com.naoek.springboot.web.dto.PostsResponseDto;
 import com.naoek.springboot.web.dto.PostsSaveRequestDto;
+import com.naoek.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,5 +25,16 @@ public class PostsApiController {
     @GetMapping("/api/v1/posts/{id}")
     public PostsResponseDto findById(@PathVariable Long id) {
         return postsService.findById(id);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id) {
+        postsService.delete(id);
+        return id;
     }
 }
